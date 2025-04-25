ba951a4530a5e20d5d8c2682dce811226efcce1b
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-04-25 19:20:33 -0300

    feat: inclui layout base da aplicação

--- src/components/UserWeather.tsx
+++ src/components/UserWeather.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { getDegrees, getDescription } from "../api/requests";
 
-const placesWeather = ["paris", "roma", "londres", "dubai"];
+import "./UserWeather.css";
+import { FiCloudRain, FiSearch, FiSun } from "react-icons/fi";
+import { HiMapPin } from "react-icons/hi2";
+
+const placesWeather = ["paris", "roma", "londres", "dubai", "indaiatuba"];
 
 const UserWeather = () => {
   const [degrees, setDegrees] = useState<number[] | null>(null);
@@ -29,18 +33,48 @@ const UserWeather = () => {
   }, []);
 
   return (
-    <>
-      {degrees?.map((degree) => (
-        <div>
-          <h2>{degree}</h2>
+    <div className="weather">
+      <div className="weather-places">
+        {degrees?.map((degree, index) => (
+          <div key={index} className="weather-temperature">
+            <div className="name-degrees">
+              <div className="name">
+                <HiMapPin className="pin" />
+                <p>{placesWeather[index]}</p>
+              </div>
+              <p className="degrees">{degree}°C</p>
+            </div>
+            <div className="icon-description">
+              <FiCloudRain className="icon" />
+              <p className="description">{descriptions?.[index]}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="weather-input">
+        <div className="input-container">
+          <form>
+            <div className="input">
+              <input type="text" placeholder="Inserir local" />
+              <button type="submit">
+                <FiSearch className="search" />
+              </button>
+            </div>
+          </form>
         </div>
-      ))}
-      {descriptions?.map((description) => (
-        <div>
-          <h2>{description}</h2>
+        <div className="user-weather">
+          <div className="user-city">
+            <HiMapPin className="icon-pin" />
+            <p>Indaiatuba</p>
+          </div>
+          <h1>27°C</h1>
+          <div className="user-degrees">
+            <FiSun className="icon-user sun" />
+            <p>Céu limpo</p>
+          </div>
         </div>
-      ))}
-    </>
+      </div>
+    </div>
   );
 };
 
