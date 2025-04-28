a76f715a83e18ce432744f13224283594bd45aee
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-04-28 16:33:05 -0300

    feat: consome API com base na resposta do usuário

--- src/components/UserWeather.tsx
+++ src/components/UserWeather.tsx
@@ -1,18 +1,23 @@
-import { useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { getDegrees, getDescription } from "../api/requests";
 
 import "./UserWeather.css";
 import { FiCloudRain, FiSearch, FiSun } from "react-icons/fi";
 import { HiMapPin } from "react-icons/hi2";
+import { BiCloud } from "react-icons/bi";
+import { BsCloudSnow } from "react-icons/bs";
 
-const placesWeather = ["paris", "roma", "londres", "dubai", "indaiatuba"];
+const placesWeather = ["Paris", "Moscou", "Londres", "Dubai", "Indaiatuba"];
 
 const UserWeather = () => {
   const [degrees, setDegrees] = useState<number[] | null>(null);
   const [descriptions, setDescriptions] = useState<string[] | null>(null);
+  const [userCity, setUserCity] = useState<string>("");
+  const [degreesUserCity, setDegreesUserCity] = useState<number>();
+  const [descriptionUserCity, setDescriptionUserCity] = useState<string>("");
 
   useEffect(() => {
-    const gettingDegrees = async () => {
+    const handleDegrees = async () => {
       const data = await Promise.all(
         placesWeather.map((place) => getDegrees(place))
       );
@@ -20,7 +25,7 @@ const UserWeather = () => {
       setDegrees(data);
     };
 
-    const gettingDescriptions = async () => {
+    const handleDescriptions = async () => {
       const data = await Promise.all(
         placesWeather.map((place) => getDescription(place))
       );
@@ -28,9 +33,55 @@ const UserWeather = () => {
       setDescriptions(data);
     };
 
-    gettingDegrees();
-    gettingDescriptions();
+    handleDegrees();
+    handleDescriptions();
   }, []);
+
+  const handleWeatherUserCity = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const degree = await getDegrees(userCity);
+
+    const description = await getDescription(userCity);
+
+    setDegreesUserCity(degree);
+
+    setDescriptionUserCity(description);
+  };
+
+  const getIconWeather = (description: string): ReactElement | null => {
+    switch (description) {
+      case "céu limpo":
+        return <FiSun className="icon sun" />;
+
+      case "algumas nuvens":
+        return <FiSun className="icon sun" />;
+
+      case "nuvens dispersas":
+        return <FiSun className="icon sun" />;
+
+      case "nuvens quebradas":
+        return <FiSun className="icon sun" />;
+
+      case "neve":
+        return <BsCloudSnow className="icon" />;
+
+      case "névoa":
+        return <BiCloud className="icon" />;
+
+      case "nublado":
+        return <BiCloud className="icon" />;
+
+      case "pouca neve":
+        return <BsCloudSnow className="icon" />;
+
+      case "neve pesada":
+        return <BsCloudSnow className="icon" />;
+
+      default:
+        return <FiCloudRain className="icon" />;
+    }
+  };
 
   return (
     <div className="weather">
@@ -45,7 +96,7 @@ const UserWeather = () => {
               <p className="degrees">{degree}°C</p>
             </div>
             <div className="icon-description">
-              <FiCloudRain className="icon" />
+              {descriptions && getIconWeather(descriptions[index])}
               <p className="description">{descriptions?.[index]}</p>
             </div>
           </div>
@@ -53,26 +104,32 @@ const UserWeather = () => {
       </div>
       <div className="weather-input">
         <div className="input-container">
-          <form>
+          <form onSubmit={handleWeatherUserCity}>
             <div className="input">
-              <input type="text" placeholder="Inserir local" />
+              <input
+                type="text"
+                placeholder="Inserir local"
+                onChange={(e) => setUserCity(e.target.value)}
+              />
               <button type="submit">
                 <FiSearch className="search" />
               </button>
             </div>
           </form>
         </div>
-        <div className="user-weather">
-          <div className="user-city">
-            <HiMapPin className="icon-pin" />
-            <p>Indaiatuba</p>
+        {degreesUserCity && (
+          <div className="user-weather">
+            <div className="user-city">
+              <HiMapPin className="icon-pin" />
+              <p>{userCity}</p>
+            </div>
+            <h1>{degreesUserCity}°C</h1>
+            <div className="user-degrees">
+              {descriptionUserCity && getIconWeather(descriptionUserCity)}
+              <p>{descriptionUserCity}</p>
+            </div>
           </div>
-          <h1>27°C</h1>
-          <div className="user-degrees">
-            <FiSun className="icon-user sun" />
-            <p>Céu limpo</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
